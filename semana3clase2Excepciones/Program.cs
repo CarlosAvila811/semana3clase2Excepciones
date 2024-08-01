dea3ded6e62b375fd65c135b40695bc6ebb5889b
CarlosAvila811 <ca03899@gmail.com>
2024-07-31 20:44:26 -0600

    Agregar archivos de proyecto.

---
+++ semana3clase2Excepciones/Program.cs
@@ -0,0 +1,16 @@
+﻿FileStream file = null;
+
+try
+{
+    double n1, n2, division;
+    Console.WriteLine("Ingrese N1");
+    n1=double.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese N2");
+    n2 = double.Parse(Console.ReadLine());
+
+    division= n1/n2;
+    Console.WriteLine($ "La division es { division}");
+} catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
